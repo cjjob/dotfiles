0eeee389c6ae7eed089e0b70adc5b374f43d9f77
Conor O'Brien <obriencjj@gmail.com>
2025-05-18 20:22:10 +0100

    Use eza and set up aliases.

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,6 +1,14 @@
 alias c="clear"
 alias cd="z"
 alias cdi="zi"
+alias l='eza --color=always --color-scale=all --color-scale-mode=gradient \
+	--icons=always --group-directories-first'
+alias la='eza --color=always --color-scale=all --color-scale-mode=gradient \
+	--icons=always --group-directories-first -a'
+alias ll='eza --color=always --color-scale=all --color-scale-mode=gradient \
+	--icons=always --group-directories-first -l --git -h'
+alias lla='eza --color=always --color-scale=all --color-scale-mode=gradient \
+	--icons=always --group-directories-first -a -l --git -h'
 alias lg="lazygit"
 alias n="nvim"
 alias srcz="source $ZDOTDIR/.zshrc"
